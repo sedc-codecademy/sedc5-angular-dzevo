be9da2bdd97ab0bacade3eb626ac38422d76a54c
xhevatibraimi <xhevat.ibraimi@gmail.com>
2017-09-22 19:41:44 +0200

    added component

--- group-mk/first-app/src/app/app.module.ts
+++ group-mk/first-app/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
-@NgModule({
+import { ProductListComponent } from "./product/product-list.component";
+import { GeneratedComponent} from "./generated/generated.component";
+let moduleInfo = {
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    GeneratedComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [ AppComponent, ProductListComponent,
+  GeneratedComponent ]
+};
+
+@NgModule(moduleInfo)
 export class AppModule { }
---
+++ group-mk/first-app/src/app/generated/generated.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generated',
+  templateUrl: './generated.component.html',
+  styleUrls: [ './generated.component.css' ]
+})
+export class GeneratedComponent {
+
+
+}

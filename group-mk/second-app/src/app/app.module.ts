fd03c8a5ae79661a6b7aaab1d2d5433bd9e43251
xhevatibraimi <xhevat.ibraimi@gmail.com>
2017-09-30 17:34:35 +0200

    added reactive forms

--- group-mk/second-app/src/app/app.module.ts
+++ group-mk/second-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -15,7 +15,8 @@ import { TodoListComponent } from "../components/todo-list/todo-list.component";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- group-mk/second-app/src/components/todo/todo.component.ts
+++ group-mk/second-app/src/components/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ViewChild, Output, EventEmitter } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { NgForm, FormControl, FormGroup } from "@angular/forms";
 import { Todo } from "../../models/todo"
 
 @Component({
@@ -8,31 +8,51 @@ import { Todo } from "../../models/todo"
 })
 export class TodoComponent implements OnInit {
 
-    @ViewChild("todoForm")
-    todoForm: NgForm;
+    @Input()
+    todoItem: Todo;
+    todoForm = new FormGroup({
+        title: new FormControl(),
+        description: new FormControl()
+    });
+
+    // @ViewChild("todoForm")
+    // todoForm: NgForm;
 
     @Output()
     todoSubmited: EventEmitter<Todo> = new EventEmitter<Todo>();
     @Output()
     todoCanceled: EventEmitter<any> = new EventEmitter<any>();
 
-    @Input()
-    todoItem: Todo;
+    handleSubmit(event) {
+        console.log(event);
+    }
 
     isControlValid(ngFormReference: NgForm, controlName: string): boolean {
         return (ngFormReference.controls[ controlName ] && ngFormReference.controls[ controlName ].valid) ? true : false;
     }
+    
     constructor() {
         this.todoItem = new Todo();
     }
 
     ngOnInit() {
+        console.log(this.todoItem);
+        this.todoForm.setValue({
+            title: this.todoItem.title,
+            description: this.todoItem.description
+        });
     }
+
     cancel() {
         this.todoCanceled.emit();
     }
     submit() {
-        this.todoSubmited.emit(this.todoItem);
+        var todo = new Todo();
+        todo.id = this.todoItem.id;
+        todo.title = this.todoForm.value.title;
+        todo.description = this.todoForm.value.description;
+        console.log(todo);
+        this.todoSubmited.emit(todo);
     }
 
 }

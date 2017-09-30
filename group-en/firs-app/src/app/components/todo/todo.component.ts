2bb94145a975b5b3d53032df5c21e6fba1ae4ff6
xhevatibraimi <xhevat.ibraimi@gmail.com>
2017-09-30 11:51:07 +0200

    added form validation

--- group-en/firs-app/src/app/components/todo/todo.component.ts
+++ group-en/firs-app/src/app/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { ToDo } from "../../../models";
 
 @Component({
@@ -9,11 +10,31 @@ export class TodoComponent implements OnInit {
     public static idCounter: number = 1;
     @Input()
     todo: ToDo;
+    
+    @ViewChild('todoForm')
+    ngFormElement: HTMLFormElement;
+
+    @ViewChild('todoForm')
+    ngForm: NgForm;
+
+    submit() {
+        console.log(this.ngForm)
+    }
+    handleKeyUp(event: HTMLFormElement) {
+        console.log(event);
+
+        this.ngOnInit();
+    }
+
+     info (){
+         console.log(this.ngFormElement);
+         console.log(this.ngFormElement.classList);
+        return this.ngFormElement.classList;
+    }
 
     ngOnInit(): void {
         this.todo = new ToDo();
         this.todo.id = "asdasdasd" + TodoComponent.idCounter++;
-
 
     }
 }

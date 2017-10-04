9dc468928b72b06b5d630de8ea03cb647572e65e
xhevatibraimi <xhevat.ibraimi@gmail.com>
2017-10-04 20:09:55 +0200

    added gx bootstrap

--- group-mk/third-app/src/app/app.module.ts
+++ group-mk/third-app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
-
+import { BsDatepickerModule } from 'ngx-bootstrap';
+// or
 import { AppComponent } from './app.component';
 import { CreateContactConponent } from "../components/create-contact/create-contact.conponent";
 @NgModule({
@@ -12,7 +13,8 @@ import { CreateContactConponent } from "../components/create-contact/create-cont
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [ AppComponent ]
--- group-mk/third-app/src/components/create-contact/create-contact.conponent.ts
+++ group-mk/third-app/src/components/create-contact/create-contact.conponent.ts
@@ -1,12 +1,36 @@
 import { Component, ViewChild, Input } from "@angular/core";
 import { NgForm, FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 import { Contact } from "../../models/contact";
+
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+// or
 @Component({
     selector: "create-contact",
     templateUrl: "./create-contact.conponent.html"
 })
 export class CreateContactConponent {
+    minDate = new Date(2017, 5, 10);
+    maxDate = new Date(2018, 9, 15);
+    _bsValue: Date;
+    get bsValue(): Date {
+        return this._bsValue;
+    }
 
+    set bsValue(v: Date) {
+        console.log(v);
+        this._bsValue = v;
+    }
+
+    _bsRangeValue: any = [ new Date(2017, 7, 4), new Date(2017, 7, 20) ];
+    get bsRangeValue(): any {
+        return this._bsRangeValue;
+    }
+
+    set bsRangeValue(v: any) {
+        this._bsRangeValue = v;
+    }
+
+    log(v: any) { console.log(v); }
     constructor(private fb: FormBuilder) {
 
     }
@@ -16,6 +40,11 @@ export class CreateContactConponent {
         phoneNumber: [ "", [ Validators.required ] ]
     });
 
+
+    createContact(e): void {
+        console.log(e);
+        console.log(this.contactForm.value);
+    }
     @Input()
     contact: Contact = Contact.empty();
 }

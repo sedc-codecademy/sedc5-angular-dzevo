bd8bfac826e735fba2e1bd46161ff7ea1eb4821d
xhevatibraimi <xhevat.ibraimi@gmail.com>
2017-10-10 20:06:04 +0200

    added contact service with http

--- group-en/second-app/src/app/services/contacts.service.ts
+++ group-en/second-app/src/app/services/contacts.service.ts
@@ -1,26 +1,22 @@
 import { Contact } from "../models/contact";
 import { Http } from "@angular/http";
 import { Observable } from "rxjs";
-import "rxjs/add/operator/map";
+import { } from "rxjs/add/operator/map";
 import { Injectable } from "@angular/core";
 import { mockContacts } from "./mock/mock-contacts";
 
 
 @Injectable()
 export class ContactsService {
+
     constructor(private http: Http) {
 
     }
 
     public getAll() {
-        return this.http.get("http://10.80.30.35:3000/contacts")
-            .map(res => {
-                var result = res.json() as Contact[];
-                console.log(result);
-                return result;
-            }, error => {
-                console.warn(error);
-            });
+        return this.http.get("http://10.80.30.195:3000/contacts")
+            .map(res => res.json() as Contact[],
+            error => { console.warn(error); });
     }
 
     public get(id: string): Observable<Contact> {
